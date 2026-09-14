d688ff40502f1b44c505ccb77069b29e0daa22db
Carson <cmorapps@gmail.com>
2026-09-14 12:36:22 -0400

    Add public raid encounter outcome contract

---
+++ src/main/java/com/cobbleraids/api/RaidEncounterOutcome.java
@@ -0,0 +1,8 @@
+package com.cobbleraids.api;
+
+/** Stable public terminal outcomes for addon-managed CobbleRaids encounters. */
+public enum RaidEncounterOutcome {
+    VICTORY,
+    DEFEAT,
+    ABORTED
+}
